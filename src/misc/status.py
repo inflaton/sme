177e4f4fdafee127298798eeac39c88fee291158
Donghao Huang <donghao.huang@mastercard.com>
2025-03-26 08:21:52 +0000

    fixed bug

--- src/misc/status.py
+++ src/misc/status.py
@@ -1,4 +1,5 @@
-from sys import argv
+import sys
+import os
 from metrics import calculate_metrics, get_metrics
 
 
@@ -9,11 +10,21 @@ def print_metrics(msg, metrics):
     print()
 
 
+def print_usage():
+    print("Usage: python status.py <path_to_directory_containing_db>")
+
+
 if __name__ == "__main__":
-    db_filepath = argv[1]
-    print(f"Calculating metrics for {db_filepath}...")
+    if len(sys.argv) < 2:
+        print("Error: No directory path provided.")
+        print_usage()
+        sys.exit(1)
+
+    directory = sys.argv[1]
+    db_filepath = os.path.join(directory, "emails.db")
+    print(f"Calculating metrics for {db_filepath}...\n")
 
     metrics = calculate_metrics(db_filepath, including_df=True)
-    df = metrics["df"]
-    del metrics["df"]
-    print_metrics("metrics:", metrics)
+    df = metrics.pop("df", None)
+
+    print_metrics("Metrics:", metrics)
