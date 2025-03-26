8839f3bd4dd9265a2e7e91c85891c78581288aa5
Donghao Huang <inflaton.sg@gmail.com>
2025-03-26 15:21:32 +0800

    added script for Calculation of Metrics

--- README.md
+++ README.md
@@ -272,27 +272,24 @@ The data under the following headers will be populated after the run:
 - reconciliation_state
 - email_details
 
-#### Final Metrics Calculation
-1. *Organize Result Files*: Move the contents of the src/data/db/* subdirectories into the appropriate results folders based on the following mapping:
+#### Calculation and Visualization of Metrics
 
-```python
-results_mapping = {
-    "RTX A6000": {
-        "qwen2.5:7b": "results/llama3.2-vision_11b-qwen2.5_7b",
-        "functionary-small": "results/llama3.2-vision_11b-functionary-small",
-        "qwen2.5:32b": "results/llama3.2-vision_11b-qwen2.5_32b",
-        "functionary-medium": "results/llama3.2-vision_11b-functionary-medium",
-        "qwen2.5:72b": "results/llama3.2-vision_11b-qwen2.5_72b",
-    },
-    "RTX 4090": {
-        "qwen2.5:7b": "results/llama3.2-vision_11b-qwen2.5_7b-RTX4090",
-        "functionary-small": "results/llama3.2-vision_11b-functionary-small-RTX4090",
-    },
-    "OpenAI": {
-        "gpt-4o-mini": "results/gpt-4o-mini-gpt-4o-mini",
-        "gpt-4o": "results/gpt-4o-gpt-4o",
-    },
-}
+1. *Calculation of Metrics*
+
+We’ve provided a Python script to calculate metrics from results stored under the src/data/db folder. Below is a sample run of the script:
+
+```bash
+$ python src/misc/calc_metrics.py src/data/db/RTX\ A6000 RTX\ A6000
+Calculating metrics for directory: src/data/db/RTX A6000 for platform: RTX A6000 ...
+Loading environment variables from: /Users/inflaton/code/papers/sme/.env
+Calculating metrics for model: modelqwen2.5_7b...
+Calculating metrics for model: modelfunctionary-small...
+Calculating metrics for model: modelqwen2.5_32b...
+Calculating metrics for model: modelfunctionary-medium...
+Calculating metrics for model: modelqwen2.5_72b...
+Metrics saved as src/data/db/metrics_RTX A6000.csv
 ```
 
-2. *Run Data Analysis Notebook*: Execute the Jupyter notebook located at `notebooks/01_Data_Analysis_all_models.ipynb`b. This will process the organized data, compute final metrics, and generate the corresponding plots.
+2. *Visualization of Metrics* 
+
+Open and execute the Jupyter notebook located at `notebooks/01_Data_Analysis_all_models.ipynb`. This notebook merges all the metrics files generated in the previous step and produces the plots included in the paper.
---
+++ src/misc/calc_metrics.py
@@ -0,0 +1,74 @@
+import os
+import sys
+from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+from metrics import *
+
+def print_usage():
+    print("Usage: python src/misc/calc_metrics.py <path_to_directory_containing_results> <platform>")
+
+if len(sys.argv) < 3:
+    print("Error: No directory path and/or platform provided.")
+    print_usage()
+    sys.exit(1)
+
+directory = sys.argv[1]
+platform = sys.argv[2]
+print(f"Calculating metrics for directory: {directory} for platform: {platform} ...")
+
+found_dotenv = find_dotenv(".env")
+
+if len(found_dotenv) == 0:
+    found_dotenv = find_dotenv(".env.example")
+print(f"loading env vars from: {found_dotenv}")
+load_dotenv(found_dotenv, override=True)
+
+subfolders = ["llama3.2-vision_11b-qwen2.5_7b",
+    "llama3.2-vision_11b-functionary-small",
+    "llama3.2-vision_11b-qwen2.5_32b",
+    "llama3.2-vision_11b-functionary-medium",
+    "llama3.2-vision_11b-qwen2.5_72b",
+    "gpt-4o-mini-gpt-4o-mini",
+    "gpt-4o-gpt-4o"]
+
+def total_power_in_watt(file_path):
+    df = pd.read_csv(file_path)
+    return df["CPU Package Power [W]"].mean() + df["GPU Power [W]"].mean()
+
+# Initialize an empty DataFrame
+metrics_df = pd.DataFrame()
+
+for subfolder in subfolders:
+    dir = f"{directory}/{subfolder}"
+    if os.path.isdir(dir):
+        model = subfolder.replace("gpt-4o-mini-", "").replace("gpt-4o-gpt", "gpt").replace("llama3.2-vision_11b-", "")
+        print(f"Calculating metrics for model: model{model}...")
+        
+        db_filepath = f"{dir}/emails.db"
+        metrics = calculate_metrics(db_filepath)
+        df = metrics["df"]
+        df = df[df["attachments"] != ""].copy()
+        metrics = get_metrics(df)
+
+        if platform != "OpenAI":
+            power = total_power_in_watt(
+                f"{dir}/power_with_vision.csv"
+            )  # + total_power_in_watt(f"{result}/power_without_vision.csv")
+            # power /= 2
+
+            idle_power_csv = f"results/power_idle_{platform}.csv"
+            if not Path(idle_power_csv).exists():
+                idle_power_csv = "results/power_idle.csv"
+            power -= total_power_in_watt(idle_power_csv)
+
+            metrics["power"] = power
+
+        # Add non-metric columns and metrics to a single dictionary
+        row_data = {"platform": platform, "model": model, **metrics}
+        metrics_df = pd.concat(
+            [metrics_df, pd.DataFrame([row_data])], ignore_index=True
+        )
+
+metrics_df.to_csv(f"results/metrics_{platform}.csv", index=False)
+print(f"Metrics saved as results/metrics_{platform}.csv")
